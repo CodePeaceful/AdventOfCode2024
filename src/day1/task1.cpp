95fc00aa2bcf5299272f485ed41b2fb082ee8235
Christian Schlecht <christian.schlecht@arcor.de>
2024-12-01 07:06:11 +0100

    day1 task1

--- src/day1/task1.cpp
+++ src/day1/task1.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <algorithm>
+#include <ranges>
+#include <numeric>
 
 #include "..\utility.hpp"
 
@@ -9,5 +12,18 @@ int main() {
 }
 
 int solve(std::vector<std::string> data) {
-
+    std::vector<int> list1;
+    std::vector<int> list2;
+    for (auto& line : data) {
+        auto content = split(line, ' ');
+        list1.push_back(std::stoi(content.front()));
+        list2.push_back(std::stoi(content.back()));
+    }
+    std::sort(list1.begin(), list1.end());
+    std::sort(list2.begin(), list2.end());
+    auto zip = std::views::zip(list1, list2);
+    return std::accumulate(zip.begin(), zip.end(), 0, [] (int in, auto pair) {
+        auto [x, y] = pair;
+        return in + std::abs(x - y);
+        });
 }
--- src/utility.hpp
+++ src/utility.hpp
@@ -24,3 +24,28 @@ std::vector<std::string> load() {
     }
     return data;
 }
+
+inline void ltrim(std::string& s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [] (unsigned char ch) {
+        return !std::isspace(ch);
+        }));
+}
+
+inline void rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [] (unsigned char ch) {
+        return !std::isspace(ch);
+        }).base(), s.end());
+}
+
+int findIndex(std::vector<int>& v, int val) {
+    for (int i = 0; i < v.size(); ++i) {
+
+          // When the element is found
+        if (v[i] == val) {
+            return i;
+        }
+    }
+
+      // When the element is not found
+    return -1;
+}
