12447110cf01603393ec52f6d46d50e2345943a2
Christian Schlecht <christian.schlecht@arcor.de>
2024-12-01 07:20:17 +0100

    day1 task2

--- src/day1/task2.cpp
+++ src/day1/task2.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <algorithm>
+#include <ranges>
+#include <numeric>
 
 #include "..\utility.hpp"
 
@@ -9,5 +12,14 @@ int main() {
 }
 
 int solve(std::vector<std::string> data) {
-    return data.size();
+    std::vector<int> list1;
+    std::vector<int> list2;
+    for (auto& line : data) {
+        auto content = split(line, ' ');
+        list1.push_back(std::stoi(content.front()));
+        list2.push_back(std::stoi(content.back()));
+    }
+    return std::accumulate(list1.begin(), list1.end(), 0, [&list2] (int in, int listelem) -> int {
+        return in + std::count(list2.begin(), list2.end(), listelem) * listelem;
+        });
 }
