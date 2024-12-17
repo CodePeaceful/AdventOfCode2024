32593f1538aff5a176997ecf664071f926e5bcbb
Christian Schlecht <christian.schlecht@arcor.de>
2024-12-17 05:11:53 +0100

    day 16 task 1 done very slow

--- src/day16/task1.cpp
+++ src/day16/task1.cpp
@@ -2,15 +2,19 @@
 #include <ranges>
 #include <algorithm>
 #include <thread>
+#include <map>
 
 #include "..\utility.hpp"
 
 int solve(std::vector<std::string>);
-int recurciveSplitWalk(std::vector<std::string> field, int line, int collum, int direction);
+void recurciveSplitWalk(std::vector<std::string> field, int line, int collum, int direction, int value);
 void fillThis(std::vector<std::string>& field, int line, int collum, int direction);
+void fillThisDirection(int collum, int line, std::vector<std::string>& field);
 void fillDeadEnds(std::vector<std::string>& data);
+void printField(const std::vector<std::string>& data);
 
-int threadCount = 0;
+int bestEnd = 0x10000000;
+std::map<std::pair<int, int>, int> crossings;
 
 int main() {
     std::cout << solve(load()) << '\n';
@@ -49,13 +53,13 @@ int solve(std::vector<std::string> data) {
     }
     std::vector<int> paths;
     for (int dir : openings) {
-        int cost = recurciveSplitWalk(data, is, js, dir);
+        int cost = 1;
         if (curdir != dir) {
             cost += 1000;
         }
-        paths.push_back(cost);
+        recurciveSplitWalk(data, is, js, dir, cost);
     }
-    return *std::min_element(paths.begin(), paths.end()) + 1;
+    return bestEnd;
 }
 
 void fillDeadEnds(std::vector<std::string>& data) {
@@ -80,11 +84,18 @@ void fillDeadEnds(std::vector<std::string>& data) {
     } while (changed);
 }
 
-int recurciveSplitWalk(std::vector<std::string> field, int line, int column, int direction) {
-    int value = 1;
+void printField(const std::vector<std::string>& data) {
+    for (const auto& line : data) {
+        std::cout << line << '\n';
+    }
+}
+
+void recurciveSplitWalk(std::vector<std::string> field, int line, int column, int direction, int value) {
+    ++value;
     std::vector<int> openings;
     field[line][column] = '#';
     fillThis(field, line, column, direction);
+    //printField(field);
     switch (direction) {
     case 0:
         ++column;
@@ -104,8 +115,11 @@ int recurciveSplitWalk(std::vector<std::string> field, int line, int column, int
     while (true) {
         openings.clear();
         if (field[line][column + 1] == 'E' || field[line][column - 1] == 'E' || field[line + 1][column] == 'E' || field[line - 1][column] == 'E') {
+            if (value < bestEnd) {
+                bestEnd = value;
+            }
             std::cout << "goal\n";
-            return value;
+            return;
         }
         field[line][column] = '#';
         if (field[line][column + 1] == '.') {
@@ -129,6 +143,22 @@ int recurciveSplitWalk(std::vector<std::string> field, int line, int column, int
             direction = openings.front();
             value += 1000;
         }
+        if (crossings.contains({ line, column })) {
+            if (crossings[{line, column}] < value) {
+                std::cout << "mapcancel\n";
+                return;
+            }
+            else if (crossings[{line, column}] > value + 1000) {
+                crossings[{line, column}] = value + 1000;
+            }
+        }
+        else {
+            crossings[{line, column}] = value + 1000;
+        }
+        if (value >= bestEnd) {
+            std::cout << "cancel\n";
+            return;
+        }
         switch (openings.front()) {
         case 0:
             ++column;
@@ -151,204 +181,75 @@ int recurciveSplitWalk(std::vector<std::string> field, int line, int column, int
     field[line][column] = '#';
     if (openings.size() == 0) {
         std::cout << line << ' ' << column << " dead\n";
-        return 0x10000000;
+        return;
     }
 
     std::vector<int> paths;
-    paths.reserve(4);
-    {
-        std::vector<std::jthread> threads;
-        void threadfunc(std::vector<int>::iterator index, const std::vector<std::string>field, int line, int column, int dir, int direction);
-        std::vector<int>::iterator  index;
-        for (int dir : openings) {
-            paths.push_back(0);
-            index = --paths.end();
-            if (threadCount < 16) {
-                threads.push_back(std::jthread(threadfunc, index, field, line, column, dir, direction));
-            }
-            else {
-                threadfunc(index, field, line, column, dir, direction);
-            }
+
+    for (int dir : openings) {
+        int cost = value;
+        if (direction != dir) {
+            cost += 1000;
         }
+        recurciveSplitWalk(field, line, column, dir, cost);
     }
-    return value + *std::min_element(paths.begin(), paths.end());
 }
 
-void threadfunc(std::vector<int>::iterator index, const std::vector<std::string> field, int line, int column, int dir, int direction) {
-    int cost = recurciveSplitWalk(field, line, column, dir);
-    if (direction != dir) {
-        cost += 1000;
-    }
-    *index = cost;
-}
+
+
 void fillThis(std::vector<std::string>& field, int line, int collum, int direction) {
     if (direction != 0) {
-        int _column = collum + 1;
-        int _line = line;
-        std::vector<int> openings;
-        do {
-            if (field[_line][_column] == '#') {
-                break;
-            }
-            openings.clear();
-            if (field[_line][_column + 1] == '.') {
-                openings.push_back(0);
-            }
-            if (field[_line][_column - 1] == '.') {
-                openings.push_back(2);
-            }
-            if (field[_line + 1][_column] == '.') {
-                openings.push_back(1);
-            }
-            if (field[_line - 1][_column] == '.') {
-                openings.push_back(3);
-            }
-            if (openings.size() <= 1) {
-                field[_line][_column] = '#';
-                if (openings.size() == 1) {
-                    switch (openings.front()) {
-                    case 0:
-                        ++_column;
-                        break;
-                    case 1:
-                        ++_line;
-                        break;
-                    case 2:
-                        --_column;
-                        break;
-                    case 3:
-                        --_line;
-                        break;
-                    }
-                }
-            }
-        } while (openings.size() == 1);
+        fillThisDirection(collum + 1, line, field);
     }
     if (direction != 1) {
-        int _column = collum;
-        int _line = line + 1;
-        std::vector<int> openings;
-        do {
-            if (field[_line][_column] == '#') {
-                break;
-            }
-            openings.clear();
-            if (field[_line][_column + 1] == '.') {
-                openings.push_back(0);
-            }
-            if (field[_line][_column - 1] == '.') {
-                openings.push_back(2);
-            }
-            if (field[_line + 1][_column] == '.') {
-                openings.push_back(1);
-            }
-            if (field[_line - 1][_column] == '.') {
-                openings.push_back(3);
-            }
-            if (openings.size() <= 1) {
-                field[_line][_column] = '#';
-                if (openings.size() == 1) {
-                    switch (openings.front()) {
-                    case 0:
-                        ++_column;
-                        break;
-                    case 1:
-                        ++_line;
-                        break;
-                    case 2:
-                        --_column;
-                        break;
-                    case 3:
-                        --_line;
-                        break;
-                    }
-                }
-            }
-        } while (openings.size() == 1);
+        fillThisDirection(collum, line + 1, field);
     }
     if (direction != 2) {
-        int _column = collum - 1;
-        int _line = line;
-        std::vector<int> openings;
-        do {
-            if (field[_line][_column] == '#') {
-                break;
-            }
-            openings.clear();
-            if (field[_line][_column + 1] == '.') {
-                openings.push_back(0);
-            }
-            if (field[_line][_column - 1] == '.') {
-                openings.push_back(2);
-            }
-            if (field[_line + 1][_column] == '.') {
-                openings.push_back(1);
-            }
-            if (field[_line - 1][_column] == '.') {
-                openings.push_back(3);
-            }
-            if (openings.size() <= 1) {
-                field[_line][_column] = '#';
-                if (openings.size() == 1) {
-                    switch (openings.front()) {
-                    case 0:
-                        ++_column;
-                        break;
-                    case 1:
-                        ++_line;
-                        break;
-                    case 2:
-                        --_column;
-                        break;
-                    case 3:
-                        --_line;
-                        break;
-                    }
-                }
-            }
-        } while (openings.size() == 1);
+        fillThisDirection(collum - 1, line, field);
     }
     if (direction != 3) {
-        int _column = collum;
-        int _line = line - 1;
-        std::vector<int> openings;
-        do {
-            if (field[_line][_column] == '#') {
-                break;
-            }
-            openings.clear();
-            if (field[_line][_column + 1] == '.') {
-                openings.push_back(0);
-            }
-            if (field[_line][_column - 1] == '.') {
-                openings.push_back(2);
-            }
-            if (field[_line + 1][_column] == '.') {
-                openings.push_back(1);
-            }
-            if (field[_line - 1][_column] == '.') {
-                openings.push_back(3);
-            }
-            if (openings.size() <= 1) {
-                field[_line][_column] = '#';
-                if (openings.size() == 1) {
-                    switch (openings.front()) {
-                    case 0:
-                        ++_column;
-                        break;
-                    case 1:
-                        ++_line;
-                        break;
-                    case 2:
-                        --_column;
-                        break;
-                    case 3:
-                        --_line;
-                        break;
-                    }
+        fillThisDirection(collum, line - 1, field);
+    }
+}
+
+void fillThisDirection(int _column, int _line, std::vector<std::string>& field) {
+    std::vector<int> openings;
+    do {
+        if (field[_line][_column] == '#') {
+            return;
+        }
+        openings.clear();
+        if (field[_line][_column + 1] == '.') {
+            openings.push_back(0);
+        }
+        if (field[_line][_column - 1] == '.') {
+            openings.push_back(2);
+        }
+        if (field[_line + 1][_column] == '.') {
+            openings.push_back(1);
+        }
+        if (field[_line - 1][_column] == '.') {
+            openings.push_back(3);
+        }
+        if (openings.size() <= 1) {
+            field[_line][_column] = '#';
+            if (openings.size() == 1) {
+                switch (openings.front()) {
+                case 0:
+                    ++_column;
+                    break;
+                case 1:
+                    ++_line;
+                    break;
+                case 2:
+                    --_column;
+                    break;
+                case 3:
+                    --_line;
+                    break;
                 }
             }
-        } while (openings.size() == 1);
-    }
+        }
+    } while (openings.size() == 1);
 }
 
